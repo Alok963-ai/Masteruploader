1b5792ba31d37c5cbe3c94a445646945853b50ec
Alok963-ai <alokjangde6@gmail.com>
2025-05-29 03:42:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("20288951"))
-    API_HASH = os.environ.get("e8cb5fb7a475b5f5eb3b0ef0e6ca03a8")
-    AUTH_USER = os.environ.get('AUTH_USERS', '7833842279').split(',')
+    API_ID = int(os.environ.get("26572696"))
+    API_HASH = os.environ.get("67a8947a3e1b15f9ef9684286baa34cb")
+    AUTH_USER = os.environ.get('AUTH_USERS', '7903596276').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
     CREDIT = " Kakabots"
